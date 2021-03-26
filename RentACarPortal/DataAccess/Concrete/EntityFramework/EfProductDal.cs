6aee487e524e5218951c337f77bc81ce0b464225
Avni <avni.kasikci.01@gmail.com>
2021-03-27 01:27:23 +0300

    The necessary layers have been created and raised so that the project is ready for the development environment

---
+++ RentACarPortal/DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,35 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //NuGet
+    public class EfProductDal : EfEntityRepositoryBase<Product, RentACarContext>, IProductDal
+    {
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            using (RentACarContext context = new RentACarContext())
+            {
+                var result = from p in context.Product
+                             join c in context.Category
+                             on p.CategoryId equals c.CategoryId
+                             select new ProductDetailDto
+                             {
+                                 ProductId = p.ProductId,
+                                 ProductName = p.ProductName,
+                                 CategoryName = c.CategoryName,
+                                 UnitsInStock = p.UnitsInStock
+                             };
+                return result.ToList();                
+            }
+        }
+    }
+}
---
+++ RentACarPortal/DataAccess/Concrete/EntityFramework/RentACarContext.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context : Db tabloları ile proje classlarını bağlamak
+    public class RentACarContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=DbRentACar;Trusted_Connection=true");           
+        }
+
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OperationClaim> OperationClaim { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaim { get; set; }
+
+    }
+}
